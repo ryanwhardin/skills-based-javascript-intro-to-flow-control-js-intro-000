6f345adb00fe0bee76d3d6efe5af8beb8efdeb0f
Ryan Hardin <ryanwhardin@gmail.com>
2020-02-16 22:41:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age > 12 && age < 20) {
+  if (age= 13-19) {
     return `You are a teenager!`
   } else {
     return `You are not a teenager`
